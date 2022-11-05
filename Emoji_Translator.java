f40de8b7f8599b3149e18d60b35be841a49be7a2
Kooreis <kooreis.official@gmail.com>
2022-11-05 09:00:00 -0500

    Populating the emojiMap with key-value pairs of text emoticons and their corresponding unicode emojis.

--- Emoji_Translator.java
+++ Emoji_Translator.java
@@ -1,6 +1,7 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class EmojiTranslator {
-    private static final Map<String, String> emojiMap = new HashMap<>();
-}
+static {
+        emojiMap.put(":)", "\uD83D\uDE00");
+        emojiMap.put(":(", "\uD83D\uDE1E");
+        emojiMap.put(";)", "\uD83D\uDE09");
+        emojiMap.put(":D", "\uD83D\uDE04");
+        emojiMap.put(":P", "\uD83D\uDE1B");
+    }
